2e08bb2ad8b3b44690d6d78ac66a10450e2f1d55
Piramilan <65810943+Piramilan@users.noreply.github.com>
2022-07-14 22:10:29 +0530

    Add files via upload

--- src/ModelUrl.js
+++ src/ModelUrl.js
@@ -2,6 +2,9 @@
 // const URl = "https://teachablemachine.withgoogle.com/models/BBLEnW2ph/";
 
 // 4 Types
-const URL = "https://teachablemachine.withgoogle.com/models/6Y0ZOWYcC/";
+// const URL = "https://teachablemachine.withgoogle.com/models/6Y0ZOWYcC/";
+
+// Old Model
+const URL = "https://teachablemachine.withgoogle.com/models/uFboKu8oZ/";
 
 export const ModelURL = URL;
